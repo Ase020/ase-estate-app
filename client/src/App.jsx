bd1579e728523981c249a93f1c7839a053bd6ac5
felixNyalenda <felixasejunior@gmail.com>
2024-04-04 15:31:53 +0300

    navbar build

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import { Navbar } from "./components";
+import "./scss/layout.scss";
+
+function App() {
+  return (
+    <div className="layout">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/navbar/Navbar.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import "./navbar.scss";
+
+const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
+  console.log("Open: ", open);
+  return (
+    <nav className="navbar">
+      <div className="left">
+        <a href="/" className="logo">
+          <img src="/logo.png" alt="logo" className="logo-img" />
+          <span>aseEstates</span>
+        </a>
+
+        <a href="/">Home</a>
+        <a href="/">About</a>
+        <a href="/">Contact</a>
+        <a href="/">Agents</a>
+      </div>
+
+      <div className="right">
+        <a href="/">Sign In</a>
+        <a href="/" className="sign-up">
+          Sign Ip
+        </a>
+
+        <div className="menu-icon">
+          <img
+            src="/menu.png"
+            alt="menu"
+            onClick={() => setOpen((prev) => !prev)}
+          />
+        </div>
+
+        <div className={open ? "menu active" : "menu"}>
+          <a href="/">Home</a>
+          <a href="/">About</a>
+          <a href="/">Contact</a>
+          <a href="/">Agents</a>
+          <a href="/">Sign In</a>
+          <a href="/">Sign Up</a>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
