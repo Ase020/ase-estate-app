311df9a5ba9106a7836321f9b2127314654621f4
felixNyalenda <felixasejunior@gmail.com>
2024-04-08 14:20:32 +0300

    chat section

--- client/src/components/chat/Chat.jsx
+++ client/src/components/chat/Chat.jsx
@@ -1,39 +1,154 @@
-import { Link } from "react-router-dom";
+import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined";
+import { useState } from "react";
+
 import "./chat.scss";
 
 function Chat() {
+  const [chat, setChat] = useState(true);
   return (
     <div className="chat">
       <div className="messages">
         <h2>Messages</h2>
 
         <div className="messages-container">
-          <MessageCard />
-          <MessageCard />
-          <MessageCard />
-          <MessageCard />
-          <MessageCard />
-          <MessageCard />
-          <MessageCard />
-          <MessageCard />
-          <MessageCard />
-          <MessageCard />
+          {Array.from({ length: 20 }).map((_, i) => (
+            <MessageCard key={i} setChat={setChat} />
+          ))}
         </div>
       </div>
 
-      <div className="chat-box">chat-box</div>
+      {chat && (
+        <div className="chat-box">
+          <div className="top">
+            <div className="user">
+              <img
+                src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                alt=""
+              />
+              <span className="username">Felix Olali</span>
+            </div>
+            <CloseOutlinedIcon
+              fontSize="small"
+              sx={{
+                cursor: "pointer",
+              }}
+              onClick={() => setChat(null)}
+            />
+          </div>
+
+          <div className="center">
+            <div className="chat-message">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <span>1 hour ago</span>
+            </div>
+
+            <div className="chat-message my-message">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <span>1 hour ago</span>
+            </div>
+
+            <div className="chat-message">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <span>1 hour ago</span>
+            </div>
+
+            <div className="chat-message my-message">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <span>1 hour ago</span>
+            </div>
+
+            <div className="chat-message">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chat-message">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <span>1 hour ago</span>
+            </div>
+
+            <div className="chat-message my-message">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <span>1 hour ago</span>
+            </div>
+
+            <div className="chat-message">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <span>1 hour ago</span>
+            </div>
+
+            <div className="chat-message my-message">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <span>1 hour ago</span>
+            </div>
+
+            <div className="chat-message">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chat-message">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <span>1 hour ago</span>
+            </div>
+
+            <div className="chat-message my-message">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <span>1 hour ago</span>
+            </div>
+
+            <div className="chat-message">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <span>1 hour ago</span>
+            </div>
+
+            <div className="chat-message my-message">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <span>1 hour ago</span>
+            </div>
+
+            <div className="chat-message">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <span>1 hour ago</span>
+            </div>
+
+            <div className="chat-message my-message">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <span>1 hour ago</span>
+            </div>
+
+            <div className="chat-message">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <span>1 hour ago</span>
+            </div>
+
+            <div className="chat-message my-message">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <span>1 hour ago</span>
+            </div>
+          </div>
+
+          <form className="bottom">
+            <textarea className="text-area" placeholder="Type a message" />
+            <button type="submit">Send</button>
+          </form>
+        </div>
+      )}
     </div>
   );
 }
 
 export default Chat;
 
-const MessageCard = () => {
+const MessageCard = ({ setChat }) => {
   return (
-    <Link to="/profile" className="message">
+    <div
+      className="message"
+      onClick={() => {
+        setChat(true);
+      }}
+    >
       <img
         src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-        alt=""
+        alt="user-pic"
       />
       <div className="message-content">
         <span>Felix Olali</span>
@@ -42,6 +157,6 @@ const MessageCard = () => {
           Hello, hope you're well and safe.
         </em>
       </div>
-    </Link>
+    </div>
   );
 };
