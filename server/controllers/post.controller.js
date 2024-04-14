92076b9a3e02562d10fadeb7782b34add4890cb0
felixNyalenda <felixasejunior@gmail.com>
2024-04-14 22:41:01 +0300

    post controller

---
+++ server/controllers/post.controller.js
@@ -0,0 +1,47 @@
+import prisma from "../lib/prisma.js";
+
+export const getPosts = async (req, res) => {
+  try {
+    const posts = await prisma.post.findMany();
+    res.status(200).json({ message: "posts" }, posts);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Failed to get posts!" });
+  }
+};
+
+export const getPost = async (req, res) => {
+  try {
+    res.status(200).json();
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Failed to get post!" });
+  }
+};
+
+export const createPost = async (req, res) => {
+  try {
+    res.status(200).json();
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Failed to create post!" });
+  }
+};
+
+export const updatePost = async (req, res) => {
+  try {
+    res.status(200).json();
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Failed to update post!" });
+  }
+};
+
+export const deletePost = async (req, res) => {
+  try {
+    res.status(200).json();
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Failed to delete post!" });
+  }
+};
